6a40158830a6ca1d4572d63ed50261efb8fe41e0
System64 <42580501+system64MC@users.noreply.github.com>
2023-03-03 15:36:10 +0100

    Update ncimnodes.h

--- src/nimgl/private/ncimnodes.h
+++ src/nimgl/private/ncimnodes.h
@@ -1,6 +1,12 @@
 #include "imnodes/imnodes.h"
 #include "imgui/imnodes_internal.h"
 #include "cimnodes.h"
+#include "imnodes/imnodes.h"
+#include "imnodes/imnodes_internal.h"
+#include "cimnodes.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "cimgui.h"
 
 // Should open an issue in cimgui about this...
 // This isn't best practice in c++ so bear with me.
