c8bddb86e168f741d80a8cbeba19ac061cc46382
System64 <42580501+system64MC@users.noreply.github.com>
2023-03-03 15:05:38 +0100

    Update imnodes.nim

--- src/nimgl/imnodes.nim
+++ src/nimgl/imnodes.nim
@@ -135,88 +135,90 @@ type
     ImNodesMiniMapNodeHoveringCallbackUserData* {.importc: "ImNodesMiniMapNodeHoveringCallbackUserData", imnodes_header.} = pointer
 
      
-proc LinkDetachWithModifierClickCreate*(): ptr LinkDetachWithModifierClick {.importc: "EmulateThreeButtonMouse_EmulateThreeButtonMouse".}
-proc LinkDetachWithModifierClickDestroy*(self: ptr LinkDetachWithModifierClick) {.importc: "EmulateThreeButtonMouse_destroy".}
-proc ImNodesIOCreate*(): ptr ImNodesIO {.importc: "LinkDetachWithModifierClick_LinkDetachWithModifierClick".}
-proc ImNodesIODestroy*(self: ptr ImNodesIO) {.importc: "LinkDetachWithModifierClick_destroy".}
-proc ImNodesStyleCreate*(): ptr ImNodesStyle {.importc: "ImNodesIO_ImNodesIO".}
-proc ImNodesStyleDestroy*(self: ptr ImNodesStyle) {.importc: "ImNodesIO_destroy".}
-proc imnodesSetImGuiContext*(ctx: ptr ImGuiContext) {.importc: "ImNodesStyle_ImNodesStyle".}
-proc imnodesCreateContext*(): ptr ImNodesContext {.importc: "ImNodesStyle_destroy".}
-proc imnodesDestroyContext*(ctx: ptr ImNodesContext) {.importc: "imnodes_SetImGuiContext".}
-proc imnodesGetCurrentContext*(): ptr ImNodesContext {.importc: "imnodes_CreateContext".}
-proc imnodesSetCurrentContext*(ctx: ptr ImNodesContext) {.importc: "imnodes_DestroyContext".}
-proc imnodesEditorContextCreate*(): ptr ImNodesEditorContext {.importc: "imnodes_GetCurrentContext".}
-proc imnodesEditorContextFree*(noname1: ptr ImNodesEditorContext) {.importc: "imnodes_SetCurrentContext".}
-proc imnodesEditorContextSet*(noname1: ptr ImNodesEditorContext) {.importc: "imnodes_EditorContextCreate".}
-proc imnodesEditorContextGetPanning*(pOut: ptr ImVec2) {.importc: "imnodes_EditorContextFree".}
-proc imnodesEditorContextResetPanning*(pos: ImVec2) {.importc: "imnodes_EditorContextSet".}
-proc imnodesEditorContextMoveToNode*(node_id: cint) {.importc: "imnodes_EditorContextGetPanning".}
-proc imnodesGetIO*(): ptr ImNodesIO {.importc: "imnodes_EditorContextResetPanning".}
-proc imnodesGetStyle*(): ptr ImNodesStyle {.importc: "imnodes_EditorContextMoveToNode".}
-proc imnodesStyleColorsDark*() {.importc: "imnodes_GetIO".}
-proc imnodesStyleColorsClassic*() {.importc: "imnodes_GetStyle".}
-proc imnodesStyleColorsLight*() {.importc: "imnodes_StyleColorsDark".}
-proc imnodesBeginNodeEditor*() {.importc: "imnodes_StyleColorsClassic".}
-proc imnodesEndNodeEditor*() {.importc: "imnodes_StyleColorsLight".}
-proc imnodesMiniMap*(minimap_size_fraction: cfloat, location: ImNodesMiniMapLocation, node_hovering_callback: ImNodesMiniMapNodeHoveringCallback, node_hovering_callback_data: ImNodesMiniMapNodeHoveringCallbackUserData) {.importc: "imnodes_BeginNodeEditor".}
-proc imnodesPushColorStyle*(item: ImNodesCol; color: cuint) {.importc: "imnodes_EndNodeEditor".}
-proc imnodesPopColorStyle*() {.importc: "imnodes_MiniMap".}
-proc imnodesPushStyleVar_Float*(style_item: ImNodesStyleVar; value: cfloat) {.importc: "imnodes_PushColorStyle".}
-proc imnodesPushStyleVar_Vec2*(style_item: ImNodesStyleVar; value: ImVec2) {.importc: "imnodes_PopColorStyle".}
-proc imnodesPopStyleVar*(count: cint) {.importc: "imnodes_PushStyleVar_Float".}
-proc imnodesBeginNode*(id: cint) {.importc: "imnodes_PushStyleVar_Vec2".}
-proc imnodesEndNode*() {.importc: "imnodes_PopStyleVar".}
-proc imnodesGetNodeDimensions*(pOut: ptr ImVec2; id: cint) {.importc: "imnodes_BeginNode".}
-proc imnodesBeginNodeTitleBar*() {.importc: "imnodes_EndNode".}
-proc imnodesEndNodeTitleBar*() {.importc: "imnodes_GetNodeDimensions".}
-proc imnodesBeginInputAttribute*(id: cint; shape: ImNodesPinShape) {.importc: "imnodes_BeginNodeTitleBar".}
-proc imnodesEndInputAttribute*() {.importc: "imnodes_EndNodeTitleBar".}
-proc imnodesBeginOutputAttribute*(id: cint; shape: ImNodesPinShape) {.importc: "imnodes_BeginInputAttribute".}
-proc imnodesEndOutputAttribute*() {.importc: "imnodes_EndInputAttribute".}
-proc imnodesBeginStaticAttribute*(id: cint) {.importc: "imnodes_BeginOutputAttribute".}
-proc imnodesEndStaticAttribute*() {.importc: "imnodes_EndOutputAttribute".}
-proc imnodesPushAttributeFlag*(flag: ImNodesAttributeFlags) {.importc: "imnodes_BeginStaticAttribute".}
-proc imnodesPopAttributeFlag*() {.importc: "imnodes_EndStaticAttribute".}
-proc imnodesLink*(id: cint; start_attribute_id: cint; end_attribute_id: cint) {.importc: "imnodes_PushAttributeFlag".}
-proc imnodesSetNodeDraggable*(node_id: cint; draggable: bool) {.importc: "imnodes_PopAttributeFlag".}
-proc imnodesSetNodeScreenSpacePos*(node_id: cint; screen_space_pos: ImVec2) {.importc: "imnodes_Link".}
-proc imnodesSetNodeEditorSpacePos*(node_id: cint; editor_space_pos: ImVec2) {.importc: "imnodes_SetNodeDraggable".}
-proc imnodesSetNodeGridSpacePos*(node_id: cint; grid_pos: ImVec2) {.importc: "imnodes_SetNodeScreenSpacePos".}
-proc imnodesGetNodeScreenSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_SetNodeEditorSpacePos".}
-proc imnodesGetNodeEditorSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_SetNodeGridSpacePos".}
-proc imnodesGetNodeGridSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_GetNodeScreenSpacePos".}
-proc imnodesIsEditorHovered*(): bool {.importc: "imnodes_GetNodeEditorSpacePos".}
-proc imnodesIsNodeHovered*(node_id: ptr cint): bool {.importc: "imnodes_GetNodeGridSpacePos".}
-proc imnodesIsLinkHovered*(link_id: ptr cint): bool {.importc: "imnodes_IsEditorHovered".}
-proc imnodesIsPinHovered*(attribute_id: ptr cint): bool {.importc: "imnodes_IsNodeHovered".}
-proc imnodesNumSelectedNodes*(): cint {.importc: "imnodes_IsLinkHovered".}
-proc imnodesNumSelectedLinks*(): cint {.importc: "imnodes_IsPinHovered".}
-proc imnodesGetSelectedNodes*(node_ids: ptr cint) {.importc: "imnodes_NumSelectedNodes".}
-proc imnodesGetSelectedLinks*(link_ids: ptr cint) {.importc: "imnodes_NumSelectedLinks".}
-proc imnodesClearNodeSelection_Nil*() {.importc: "imnodes_GetSelectedNodes".}
-proc imnodesClearLinkSelection_Nil*() {.importc: "imnodes_GetSelectedLinks".}
-proc imnodesSelectNode*(node_id: cint) {.importc: "imnodes_ClearNodeSelection_Nil".}
-proc imnodesClearNodeSelection_Int*(node_id: cint) {.importc: "imnodes_ClearLinkSelection_Nil".}
-proc imnodesIsNodeSelected*(node_id: cint): bool {.importc: "imnodes_SelectNode".}
-proc imnodesSelectLink*(link_id: cint) {.importc: "imnodes_ClearNodeSelection_Int".}
-proc imnodesClearLinkSelection_Int*(link_id: cint) {.importc: "imnodes_IsNodeSelected".}
-proc imnodesIsLinkSelected*(link_id: cint): bool {.importc: "imnodes_SelectLink".}
-proc imnodesIsAttributeActive*(): bool {.importc: "imnodes_ClearLinkSelection_Int".}
-proc imnodesIsAnyAttributeActive*(attribute_id: ptr cint): bool {.importc: "imnodes_IsLinkSelected".}
-proc imnodesIsLinkStarted*(started_at_attribute_id: ptr cint): bool {.importc: "imnodes_IsAttributeActive".}
-proc imnodesIsLinkDropped*(started_at_attribute_id: ptr cint, including_detached_links: bool): bool {.importc: "imnodes_IsAnyAttributeActive".}
-proc imnodesIsLinkCreated_BoolPtr*(started_at_attribute_id: ptr cint, ended_at_attribute_id: ptr cint, created_from_snap: ptr bool): bool {.importc: "imnodes_IsLinkStarted".}
-proc imnodesIsLinkCreated_IntPtr*(started_at_node_id: ptr cint, started_at_attribute_id: ptr cint, ended_at_node_id: ptr cint, ended_at_attribute_id: ptr cint, created_from_snap: ptr bool): bool {.importc: "imnodes_IsLinkDropped".}
-proc imnodesIsLinkDestroyed*(link_id: ptr cint): bool {.importc: "imnodes_IsLinkCreated_BoolPtr".}
-proc imnodesSaveCurrentEditorStateToIniString*(data_size: ptr csize_t): cstring {.importc: "imnodes_IsLinkCreated_IntPtr".}
-proc imnodesSaveEditorStateToIniString*(editor: ptr ImNodesEditorContext, data_size: ptr csize_t): cstring {.importc: "imnodes_IsLinkDestroyed".}
-proc imnodesLoadCurrentEditorStateFromIniString*(data: cstring; data_size: csize_t) {.importc: "imnodes_SaveCurrentEditorStateToIniString".}
-proc imnodesLoadEditorStateFromIniString*(editor: ptr ImNodesEditorContext, data: cstring; data_size: csize_t) {.importc: "imnodes_SaveEditorStateToIniString".}
-proc imnodesSaveCurrentEditorStateToIniFile*(file_name: cstring) {.importc: "imnodes_LoadCurrentEditorStateFromIniString".}
-proc imnodesSaveEditorStateToIniFile*(editor: ptr ImNodesEditorContext, file_name: cstring) {.importc: "imnodes_LoadEditorStateFromIniString".}
-proc imnodesLoadCurrentEditorStateFromIniFile*(file_name: cstring) {.importc: "imnodes_SaveCurrentEditorStateToIniFile".}
-proc imnodesLoadEditorStateFromIniFile*(editor: ptr ImNodesEditorContext, file_name: cstring) {.importc: "imnodes_SaveEditorStateToIniFile".}
+proc EmulateThreeButtonMouseCreate*(): ptr EmulateThreeButtonMouse {.importc: "EmulateThreeButtonMouse_EmulateThreeButtonMouse".}
+proc EmulateThreeButtonMouseDestroy*(self: ptr EmulateThreeButtonMouse) {.importc: "EmulateThreeButtonMouse_destroy".}
+proc LinkDetachWithModifierClick_LinkDetachWithModifierClick*(): ptr LinkDetachWithModifierClick {.importc: "LinkDetachWithModifierClick_LinkDetachWithModifierClick".}
+proc LinkDetachWithModifierClick_destroy*(self: ptr LinkDetachWithModifierClick) {.importc: "LinkDetachWithModifierClick_destroy".}
+proc ImNodesIOCreate*(): ptr ImNodesIO {.importc: "ImNodesIO_ImNodesIO".}
+proc ImNodesIODestroy*(self: ptr ImNodesIO) {.importc: "ImNodesIO_destroy".}
+proc ImNodesStyleCreate*(): ptr ImNodesStyle {.importc: "ImNodesStyle_ImNodesStyle".}
+proc ImNodesStyleDestroy*(self: ptr ImNodesStyle) {.importc: "ImNodesStyle_destroy".}
+proc imnodesSetImGuiContext*(ctx: ptr ImGuiContext) {.importc: "imnodes_SetImGuiContext".}
+proc imnodesCreateContext*(): ptr ImNodesContext {.importc: "imnodes_CreateContext".}
+proc imnodesDestroyContext*(ctx: ptr ImNodesContext) {.importc: "imnodes_DestroyContext".}
+proc imnodesGetCurrentContext*(): ptr ImNodesContext {.importc: "imnodes_GetCurrentContext".}
+proc imnodesSetCurrentContext*(ctx: ptr ImNodesContext) {.importc: "imnodes_SetCurrentContext".}
+proc imnodesEditorContextCreate*(): ptr ImNodesEditorContext {.importc: "imnodes_EditorContextCreate".}
+proc imnodesEditorContextFree*(noname1: ptr ImNodesEditorContext) {.importc: "imnodes_EditorContextFree".}
+proc imnodesEditorContextSet*(noname1: ptr ImNodesEditorContext) {.importc: "imnodes_EditorContextSet".}
+proc imnodesEditorContextGetPanning*(pOut: ptr ImVec2) {.importc: "imnodes_EditorContextGetPanning".}
+proc imnodesEditorContextResetPanning*(pos: ImVec2) {.importc: "imnodes_EditorContextResetPanning".}
+proc imnodesEditorContextMoveToNode*(node_id: cint) {.importc: "imnodes_EditorContextMoveToNode".}
+proc imnodesGetIO*(): ptr ImNodesIO {.importc: "imnodes_GetIO".}
+proc imnodesGetStyle*(): ptr ImNodesStyle {.importc: "imnodes_GetStyle".}
+proc imnodesStyleColorsDark*() {.importc: "imnodes_StyleColorsDark".}
+proc imnodesStyleColorsClassic*() {.importc: "imnodes_StyleColorsClassic".}
+proc imnodesStyleColorsLight*() {.importc: "imnodes_StyleColorsLight".}
+proc imnodesBeginNodeEditor*() {.importc: "imnodes_BeginNodeEditor".}
+proc imnodesEndNodeEditor*() {.importc: "imnodes_EndNodeEditor".}
+proc imnodesMiniMap*(minimap_size_fraction: cfloat, location: ImNodesMiniMapLocation, node_hovering_callback: ImNodesMiniMapNodeHoveringCallback, node_hovering_callback_data: ImNodesMiniMapNodeHoveringCallbackUserData) {.importc: "imnodes_MiniMap".}
+proc imnodesPushColorStyle*(item: ImNodesCol; color: cuint) {.importc: "imnodes_PushColorStyle".}
+proc imnodesPopColorStyle*() {.importc: "imnodes_PopColorStyle".}
+proc imnodesPushStyleVar_Float*(style_item: ImNodesStyleVar; value: cfloat) {.importc: "imnodes_PushStyleVar_Float".}
+proc imnodesPushStyleVar_Vec2*(style_item: ImNodesStyleVar; value: ImVec2) {.importc: "imnodes_PushStyleVar_Vec2".}
+proc imnodesPopStyleVar*(count: cint) {.importc: "imnodes_PopStyleVar".}
+proc imnodesBeginNode*(id: cint) {.importc: "imnodes_BeginNode".}
+proc imnodesEndNode*() {.importc: "imnodes_EndNode".}
+proc imnodesGetNodeDimensions*(pOut: ptr ImVec2; id: cint) {.importc: "imnodes_GetNodeDimensions".}
+proc imnodesBeginNodeTitleBar*() {.importc: "imnodes_BeginNodeTitleBar".}
+proc imnodesEndNodeTitleBar*() {.importc: "imnodes_EndNodeTitleBar".}
+proc imnodesBeginInputAttribute*(id: cint; shape: ImNodesPinShape) {.importc: "imnodes_BeginInputAttribute".}
+proc imnodesEndInputAttribute*() {.importc: "imnodes_EndInputAttribute".}
+proc imnodesBeginOutputAttribute*(id: cint; shape: ImNodesPinShape) {.importc: "imnodes_BeginOutputAttribute".}
+proc imnodesEndOutputAttribute*() {.importc: "imnodes_EndOutputAttribute".}
+proc imnodesBeginStaticAttribute*(id: cint) {.importc: "imnodes_BeginStaticAttribute".}
+proc imnodesEndStaticAttribute*() {.importc: "imnodes_EndStaticAttribute".}
+proc imnodesPushAttributeFlag*(flag: ImNodesAttributeFlags) {.importc: "imnodes_PushAttributeFlag".}
+proc imnodesPopAttributeFlag*() {.importc: "imnodes_PopAttributeFlag".}
+proc imnodesLink*(id: cint; start_attribute_id: cint; end_attribute_id: cint) {.importc: "imnodes_Link".}
+proc imnodesSetNodeDraggable*(node_id: cint; draggable: bool) {.importc: "imnodes_SetNodeDraggable".}
+proc imnodesSetNodeScreenSpacePos*(node_id: cint; screen_space_pos: ImVec2) {.importc: "imnodes_SetNodeScreenSpacePos".}
+proc imnodesSetNodeEditorSpacePos*(node_id: cint; editor_space_pos: ImVec2) {.importc: "imnodes_SetNodeEditorSpacePos".}
+proc imnodesSetNodeGridSpacePos*(node_id: cint; grid_pos: ImVec2) {.importc: "imnodes_SetNodeGridSpacePos".}
+proc imnodesGetNodeScreenSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_GetNodeScreenSpacePos".}
+proc imnodesGetNodeEditorSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_GetNodeEditorSpacePos".}
+proc imnodesGetNodeGridSpacePos*(pOut: ptr ImVec2; node_id: cint) {.importc: "imnodes_GetNodeGridSpacePos".}
+proc imnodesIsEditorHovered*(): bool {.importc: "imnodes_IsEditorHovered".}
+proc imnodesIsNodeHovered*(node_id: ptr cint): bool {.importc: "imnodes_IsNodeHovered".}
+proc imnodesIsLinkHovered*(link_id: ptr cint): bool {.importc: "imnodes_IsLinkHovered".}
+proc imnodesIsPinHovered*(attribute_id: ptr cint): bool {.importc: "imnodes_IsPinHovered".}
+proc imnodesNumSelectedNodes*(): cint {.importc: "imnodes_NumSelectedNodes".}
+proc imnodesNumSelectedLinks*(): cint {.importc: "imnodes_NumSelectedLinks".}
+proc imnodesGetSelectedNodes*(node_ids: ptr cint) {.importc: "imnodes_GetSelectedNodes".}
+proc imnodesGetSelectedLinks*(link_ids: ptr cint) {.importc: "imnodes_GetSelectedLinks".}
+proc imnodesClearNodeSelection_Nil*() {.importc: "imnodes_ClearNodeSelection_Nil".}
+proc imnodesClearLinkSelection_Nil*() {.importc: "imnodes_ClearLinkSelection_Nil".}
+proc imnodesSelectNode*(node_id: cint) {.importc: "imnodes_SelectNode".}
+proc imnodesClearNodeSelection_Int*(node_id: cint) {.importc: "imnodes_ClearNodeSelection_Int".}
+proc imnodesIsNodeSelected*(node_id: cint): bool {.importc: "imnodes_IsNodeSelected".}
+proc imnodesSelectLink*(link_id: cint) {.importc: "imnodes_SelectLink".}
+proc imnodesClearLinkSelection_Int*(link_id: cint) {.importc: "imnodes_ClearLinkSelection_Int".}
+proc imnodesIsLinkSelected*(link_id: cint): bool {.importc: "imnodes_IsLinkSelected".}
+proc imnodesIsAttributeActive*(): bool {.importc: "imnodes_IsAttributeActive".}
+proc imnodesIsAnyAttributeActive*(attribute_id: ptr cint): bool {.importc: "imnodes_IsAnyAttributeActive".}
+proc imnodesIsLinkStarted*(started_at_attribute_id: ptr cint): bool {.importc: "imnodes_IsLinkStarted".}
+proc imnodesIsLinkDropped*(started_at_attribute_id: ptr cint, including_detached_links: bool): bool {.importc: "imnodes_IsLinkDropped".}
+proc imnodesIsLinkCreated_BoolPtr*(started_at_attribute_id: ptr cint, ended_at_attribute_id: ptr cint, created_from_snap: ptr bool): bool {.importc: "imnodes_IsLinkCreated_BoolPtr".}
+proc imnodesIsLinkCreated_IntPtr*(started_at_node_id: ptr cint, started_at_attribute_id: ptr cint, ended_at_node_id: ptr cint, ended_at_attribute_id: ptr cint, created_from_snap: ptr bool): bool {.importc: "imnodes_IsLinkCreated_IntPtr".}
+proc imnodesIsLinkDestroyed*(link_id: ptr cint): bool {.importc: "imnodes_IsLinkDestroyed".}
+proc imnodesSaveCurrentEditorStateToIniString*(data_size: ptr csize_t): cstring {.importc: "imnodes_SaveCurrentEditorStateToIniString".}
+proc imnodesSaveEditorStateToIniString*(editor: ptr ImNodesEditorContext, data_size: ptr csize_t): cstring {.importc: "imnodes_SaveEditorStateToIniString".}
+proc imnodesLoadCurrentEditorStateFromIniString*(data: cstring; data_size: csize_t) {.importc: "imnodes_LoadCurrentEditorStateFromIniString".}
+proc imnodesLoadEditorStateFromIniString*(editor: ptr ImNodesEditorContext, data: cstring; data_size: csize_t) {.importc: "imnodes_LoadEditorStateFromIniString".}
+proc imnodesSaveCurrentEditorStateToIniFile*(file_name: cstring) {.importc: "imnodes_SaveCurrentEditorStateToIniFile".}
+proc imnodesSaveEditorStateToIniFile*(editor: ptr ImNodesEditorContext, file_name: cstring) {.importc: "imnodes_SaveEditorStateToIniFile".}
+proc imnodesLoadCurrentEditorStateFromIniFile*(file_name: cstring) {.importc: "imnodes_LoadCurrentEditorStateFromIniFile".}
+proc imnodesLoadEditorStateFromIniFile*(editor: ptr ImNodesEditorContext, file_name: cstring) {.importc: "imnodes_LoadEditorStateFromIniFile".}
 
 proc getIOKeyCtrlPtr*(): ptr bool {.importc: "imnodes_SaveEditorStateToIniFile".}
 
